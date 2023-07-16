5ed0a2d5ecb08330263206ac1f1ab21cd9695c8d
ouseqqam <ouseqqam@student.1337.ma>
2023-07-16 21:02:18 +0100

    changes when add the auth repo and prisma

--- server/src/authentification/authentification.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('authentification')
-export class AuthentificationController {}
--- server/src/authentification/authentification.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthentificationService } from './authentification.service';
-import { AuthentificationController } from './authentification.controller';
-
-@Module({
-  providers: [AuthentificationService],
-  controllers: [AuthentificationController]
-})
-export class AuthentificationModule {}
